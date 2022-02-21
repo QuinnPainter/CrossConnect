37d431b2f7b66f8942fad11e97a614f3992a207e
Quinn Painter <quinn@geekymonkey.com>
2022-02-21 23:04:59 +0000

    Node tiles

--- src/main.c
+++ src/main.c
@@ -8,11 +8,13 @@
 #include "helpers.h"
 
 enum tileTypes {
-    TILE_EMPTY,
-    TILE_FILLED
+    TILE_EMPTY = 0b0000,
+    TILE_FILLED = 0b1111,
+    TILE_NODE = 0b1110
 };
 
 ASSET(tiles, "tiles.2bpp");
+ASSET(nodeNumberTiles, "nodeNumbers.2bpp");
 
 uint8_t cursor_board_prev_x = 0;
 uint8_t cursor_board_prev_y = 0;
@@ -23,33 +25,38 @@ uint8_t board[18][20];
 
 #define F TILE_FILLED
 #define E TILE_EMPTY
+#define N1 (0b00000000 + TILE_NODE)
+#define N2 (0b00010000 + TILE_NODE)
 const uint8_t initialBoard[18][20] = {
     {F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F},
     {F, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, F},
-    {F, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, F},
+    {F, N1, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, F},
     {F, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, F},
     {F, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, F},
     {F, E, E, E, E, E, E, E, F, F, E, E, E, E, E, E, E, E, E, F},
-    {F, E, E, E, E, E, E, E, F, F, E, E, E, E, E, E, E, E, E, F},
+    {F, E, E, E, E, E, E, E, F, F, E, E, E, E, E, N2, E, E, E, F},
     {F, E, E, E, E, E, E, E, E, F, E, E, E, E, E, E, E, E, E, F},
     {F, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, F},
     {F, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, F},
+    {F, E, E, E, E, E, E, E, E, E, E, N2, E, E, E, E, E, E, E, F},
     {F, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, F},
     {F, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, F},
     {F, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, F},
-    {F, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, F},
-    {F, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, F},
+    {F, E, E, E, E, E, E, E, N1, E, E, E, E, E, E, E, E, E, E, F},
     {F, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, F},
     {F, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, E, F},
     {F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F}
 };
 #undef E
 #undef F
+#undef N1
+#undef N2
 
 void main() {
     //Load our initial vram (this is slow, but always works, even outside of vblank)
     vram_memcpy(0x8000, tiles, tiles_end - tiles);
     vram_memcpy(0x9000, tiles, tiles_end - tiles);
+    vram_memcpy(0x9100, nodeNumberTiles, nodeNumberTiles_end - nodeNumberTiles);
     //Setup the OAM for sprite drawing
     oam_init();
 
@@ -61,7 +68,7 @@ void main() {
     //Put some sprites on the screen
     shadow_oam[0].y = 0x00;
     shadow_oam[0].x = 0x00;
-    shadow_oam[0].tile = 0x04;
+    shadow_oam[0].tile = 0x02;
     shadow_oam[0].attr = 0x00;
 
     //Make sure sprites and the background are drawn
@@ -82,13 +89,16 @@ void main() {
     {
         for (uint8_t x = 0; x < 20; x++)
         {
-            switch(board[y][x])
+            switch(board[y][x] & 0xF)
             {
                 case TILE_FILLED:
-                    vram_set(0x9800 + (y * 0x20) + x, 0x07);
+                    vram_set(0x9800 + (y * 0x20) + x, 0x01);
                     break;
                 case TILE_EMPTY:
-                    vram_set(0x9800 + (y * 0x20) + x, 0x05);
+                    vram_set(0x9800 + (y * 0x20) + x, 0x00);
+                    break;
+                case TILE_NODE:
+                    vram_set(0x9800 + (y * 0x20) + x, 0x10 + (board[y][x] >> 4));
                     break;
                 default: BGB_BREAKPOINT();
             }
