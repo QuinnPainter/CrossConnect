2f3a3fda7ed41055dc72c22a9b6f00e620484fcb
Quinn Painter <quinn@geekymonkey.com>
2022-03-08 21:45:57 +0000

    Script to generate puzzles using Numberlink

---
+++ levelgen/levelgen.py
@@ -0,0 +1,48 @@
+import subprocess
+import math
+
+outPuzArray = bytearray(0)
+
+for i in range(1000):
+    outPuz = bytearray(0)
+
+    width = 10
+    height = 11
+
+    startPuz = subprocess.check_output(['python', 'numberlink/gen/gen.py', '--max', '16', '--terminal-only', '--no-colors', str(width), str(height), '1'])
+
+    # remove starting line with puzzle dimensions
+    startPuz = startPuz.split(b'\n', 1)[1]
+    #print (startPuz.decode('utf-8'))
+    # remove all other newlines
+    startPuz = startPuz.decode('utf-8').replace("\n", "")
+
+    outPuz.append(0) # placeholder for node count
+    outPuz.append((width << 4) | height)
+
+    curNodeNum = 1
+    while (True):
+        curPos = 0
+        node1Pos = None
+        node2Pos = None
+        for c in startPuz:
+            if c != '.' and int(c, 16) == curNodeNum:
+                if node1Pos == None:
+                    node1Pos = curPos
+                else:
+                    node2Pos = curPos
+                    break
+            curPos += 1
+
+        if (node1Pos == None):
+            break
+        outPuz.append(((node1Pos % width) << 4) | math.floor(node1Pos / width))
+        outPuz.append(((node2Pos % width) << 4) | math.floor(node2Pos / width))
+        #print("node " + str(curNodeNum) + "  x: " + str(node1Pos % width) + "  y: " + str(math.floor(node1Pos / width)))
+        #print("node " + str(curNodeNum) + "  x: " + str(node2Pos % width) + "  y: " + str(math.floor(node2Pos / width)))
+        curNodeNum += 1
+    outPuz[0] = curNodeNum - 1
+    outPuzArray += outPuz
+    #print (outPuz.hex())
+
+print (len(outPuzArray))
