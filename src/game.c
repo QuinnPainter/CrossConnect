fe3e6e1b194d5f90a561943f24102015f1fe07da
Quinn Painter <quinn@geekymonkey.com>
2022-03-29 11:28:47 +0100

    Basic main menu function

--- src/game.c
+++ src/game.c
@@ -39,30 +39,6 @@ uint8_t cursorYOffset;
 
 uint8_t board[18][18];
 
-#define BGCOLOUR PAL24(0xFFFFFF)
-#define GRIDCOLOUR PAL24(0x999999)
-const uint16_t bgpal[8*4] = {
-    BGCOLOUR, GRIDCOLOUR, PAL24(0x000000), PAL24(0xFF0000), // black & red
-    BGCOLOUR, GRIDCOLOUR, PAL24(0xB30033), PAL24(0x00FF00), // reddish-pink & green
-    BGCOLOUR, GRIDCOLOUR, PAL24(0x002861), PAL24(0x0000FF), // navy & blue
-    BGCOLOUR, GRIDCOLOUR, PAL24(0x00692C), PAL24(0xFF7700), // dark green & orange
-    BGCOLOUR, GRIDCOLOUR, PAL24(0x5EAD82), PAL24(0x5E5E5E), // dark blue-green & grey
-    BGCOLOUR, GRIDCOLOUR, PAL24(0x704C25), PAL24(0xFF00FF), // brown & pink
-    BGCOLOUR, GRIDCOLOUR, PAL24(0xA6BF00), PAL24(0x00FFFF), // yellow & cyan
-    BGCOLOUR, GRIDCOLOUR, PAL24(0x610069), PAL24(0x6000D6), // dark purple & purple
-};
-
-const uint16_t objpal[8*4] = {
-    PAL24(0x000000), PAL24(0x000000), PAL24(0x000000), PAL24(0xFFFFFF),
-    PAL24(0x000000), PAL24(0x000000), PAL24(0x000000), PAL24(0x999999),
-    PAL24(0x000000), PAL24(0x000000), PAL24(0x000000), PAL24(0x444444),
-    PAL24(0x000000), PAL24(0x000000), PAL24(0x000000), PAL24(0x000000),
-    PAL24(0x000000), PAL24(0x000000), PAL24(0x000000), PAL24(0x000000),
-    PAL24(0x000000), PAL24(0x000000), PAL24(0x000000), PAL24(0x000000),
-    PAL24(0x000000), PAL24(0x000000), PAL24(0x000000), PAL24(0x000000),
-    PAL24(0x000000), PAL24(0x000000), PAL24(0x000000), PAL24(0x000000),
-};
-
 // Inverts a direction, while maintaining the colour value.
 uint8_t invertDirection(uint8_t dir)
 {
@@ -374,160 +350,13 @@ inline void eraseTileConnections(uint8_t x, uint8_t y)
     drawOneTile(x, y);
 }
 
-void genConnectedNodeTiles(uint8_t* startPtr, uint8_t* endPtr, bool useAltColour)
-{
-    if (useAltColour)
-    {
-        // change 01 to 10
-        for (uint8_t* i = startPtr; i < endPtr; i += 16) // top
-        {
-            *(i) &= ~0b00011000;
-            (*(i + 1)) |= 0b00011000;
-        }
-        for (uint8_t* i = startPtr + 0x010E; i < endPtr + 0x010E; i += 16) // bottom
-        {
-            (*i) &= ~0b00011000;
-            (*(i + 1)) |= 0b00011000;
-        }
-        for (uint8_t* i = startPtr + 0x0206; i < endPtr + 0x0206; i += 16) // left
-        {
-            (*i) &= ~0b10000000;
-            (*(i + 1)) |= 0b10000000;
-            (*(i + 2)) &= ~0b10000000;
-            (*(i + 3)) |= 0b10000000;
-        }
-        for (uint8_t* i = startPtr + 0x0306; i < endPtr + 0x0306; i += 16) // right
-        {
-            (*i) &= ~0b00000001;
-            (*(i + 1)) |= 0b00000001;
-            (*(i + 2)) &= ~0b00000001;
-            (*(i + 3)) |= 0b00000001;
-        }
-    }
-    else
-    {
-        // some of the writes aren't needed since there's already a grid box drawn around the node
-        // it doesn't need to change 00 to 11, just 01 to 11
-        for (uint8_t* i = startPtr; i < endPtr; i += 16) // top
-        {
-            //(*i) |= 0b00011000;
-            (*(i + 1)) |= 0b00011000;
-        }
-        for (uint8_t* i = startPtr + 0x010E; i < endPtr + 0x010E; i += 16) // bottom
-        {
-            //(*i) |= 0b00011000;
-            (*(i + 1)) |= 0b00011000;
-        }
-        for (uint8_t* i = startPtr + 0x0206; i < endPtr + 0x0206; i += 16) // left
-        {
-            //(*i) |= 0b10000000;
-            (*(i + 1)) |= 0b10000000;
-            //(*(i + 2)) |= 0b10000000;
-            (*(i + 3)) |= 0b10000000;
-        }
-        for (uint8_t* i = startPtr + 0x0306; i < endPtr + 0x0306; i += 16) // right
-        {
-            //(*i) |= 0b00000001;
-            (*(i + 1)) |= 0b00000001;
-            //(*(i + 2)) |= 0b00000001;
-            (*(i + 3)) |= 0b00000001;
-        }
-    }
-}
-
 void runGame()
 {
-    lcd_off(); // Disable screen so we can copy to VRAM freely
-
-    if (cpu_type == CPU_CGB)
-    {
-        cgb_background_palette(bgpal);
-        cgb_object_palette(objpal);
-    }
-    else if (cpu_type == CPU_DMG)
-    {
-        rBGP = 0b11100100;
-        rOBP0 = 0b11100100;
-        rOBP1 = 0b11100100;
-    }
-
-    memcpy((void*)0x8000, cursorTiles, cursorTiles_end - cursorTiles);
-    memcpy((void*)0x8800, backgroundTiles, backgroundTiles_end - backgroundTiles);
-    memcpy((void*)0x8900, connectionTiles, connectionTiles_end - connectionTiles);
-    memcpy((void*)0x8B00, nodeShapeTiles, nodeShapeTiles_end - nodeShapeTiles); // unconnected
-
-    // Modify the tiles for CGB
-    if (cpu_type == CPU_CGB)
-    {
-        memcpy((void*)0x8A00, nodeNumberTilesCGB, nodeNumberTilesCGB_end - nodeNumberTilesCGB);
-
-        // Copy in the tiles for the alternate colour connections
-        memcpy((void*)0x9000, connectionTiles, connectionTiles_end - connectionTiles);
-
-        // Convert the colours for the alternate colour connections
-        for (uint8_t* i = (uint8_t*)0x9000; i < (uint8_t*)(0x9000 + (connectionTiles_end - connectionTiles)); i += 2)
-        {
-            for (uint8_t a = 1; a != 0; a <<= 1)
-            {
-                if (((*i) & a) && ((*(i + 1)) & a))
-                {
-                    (*i) &= ~a;
-                }
-            }
-        }
-
-        // Convert the colours for the alternate colour shape nodes
-        for (uint8_t* i = (uint8_t*)0x8B80; i < (uint8_t*)(0x8B80 + 16 * 8); i += 2)
-        {
-            for (uint8_t a = 1; a != 0; a <<= 1)
-            {
-                if (((*i) & a) && ((*(i + 1)) & a))
-                {
-                    (*i) &= ~a;
-                }
-            }
-        }
-
-        memcpy((void*)0x8C00, (void*)0x8B00, nodeShapeTiles_end - nodeShapeTiles); // top
-        memcpy((void*)0x8D00, (void*)0x8B00, nodeShapeTiles_end - nodeShapeTiles); // bottom
-        memcpy((void*)0x8E00, (void*)0x8B00, nodeShapeTiles_end - nodeShapeTiles); // left
-        memcpy((void*)0x8F00, (void*)0x8B00, nodeShapeTiles_end - nodeShapeTiles); // right
-
-        // Generate the "connected" tiles for nodeShapes
-        genConnectedNodeTiles((uint8_t*)0x8C00, (uint8_t*)0x8C80, false);
-        genConnectedNodeTiles((uint8_t*)0x8C80, (uint8_t*)0x8D00, true);
-    }
-    else // DMG
-    {
-        memcpy((void*)0x8A00, nodeNumberTiles, nodeNumberTiles_end - nodeNumberTiles);
-
-        memcpy((void*)0x8C00, (void*)0x8B00, nodeShapeTiles_end - nodeShapeTiles); // top
-        memcpy((void*)0x8D00, (void*)0x8B00, nodeShapeTiles_end - nodeShapeTiles); // bottom
-        memcpy((void*)0x8E00, (void*)0x8B00, nodeShapeTiles_end - nodeShapeTiles); // left
-        memcpy((void*)0x8F00, (void*)0x8B00, nodeShapeTiles_end - nodeShapeTiles); // right
-
-        // Generate the "connected" tiles for nodeShapes
-        genConnectedNodeTiles((uint8_t*)0x8C00, (uint8_t*)0x8D00, false);
-    }
-    //Setup the OAM for sprite drawing
-    oam_init();
-
-    //Put some sprites on the screen
+    // Setup cursor sprite
     shadow_oam[0].y = 0x00;
     shadow_oam[0].x = 0x00;
     shadow_oam[0].tile = TILE_CURSOR;
     shadow_oam[0].attr = 0x00;
-
-    //Make sure sprites and the background are drawn
-    rLCDC = LCDC_ON | LCDC_OBJON | LCDC_BGON;
-
-    //Setup the VBLANK interrupt.
-    rIF = 0;
-    rIE = IE_VBLANK;
-    ENABLE_INTERRUPTS();
-
-    // Init joypad state
-    joypad_state = 0;
 
     memset(board, BOARD_TILE_FILLED, sizeof(board));
     curLevelPackAddr = testLevels;
@@ -628,10 +457,4 @@ void processMove()
     // Screen to OAM coordinates
     cursorTargetY = (cursorBoardY * 8) + cursorYOffset;
     cursorTargetX = (cursorBoardX * 8) + cursorXOffset;
-}
-
-ISR_VBLANK()
-{
-    oam_dma_copy();
-    updateCursorAnimation();
 }
--- src/main.c
+++ src/main.c
@@ -1,6 +1,211 @@
+#include "sdk/interrupt.h"
+#include "sdk/oam.h"
+#include "sdk/video.h"
+#include "sdk/system.h"
+#include "sdk/joypad.h"
+#include "sdk/hardware.h"
 #include "game.h"
+#include "cursor.h"
+#include "gameassets.h"
+
+#define BGCOLOUR PAL24(0xFFFFFF)
+#define GRIDCOLOUR PAL24(0x999999)
+const uint16_t bgpal[8*4] = {
+    BGCOLOUR, GRIDCOLOUR, PAL24(0x000000), PAL24(0xFF0000), // black & red
+    BGCOLOUR, GRIDCOLOUR, PAL24(0xB30033), PAL24(0x00FF00), // reddish-pink & green
+    BGCOLOUR, GRIDCOLOUR, PAL24(0x002861), PAL24(0x0000FF), // navy & blue
+    BGCOLOUR, GRIDCOLOUR, PAL24(0x00692C), PAL24(0xFF7700), // dark green & orange
+    BGCOLOUR, GRIDCOLOUR, PAL24(0x5EAD82), PAL24(0x5E5E5E), // dark blue-green & grey
+    BGCOLOUR, GRIDCOLOUR, PAL24(0x704C25), PAL24(0xFF00FF), // brown & pink
+    BGCOLOUR, GRIDCOLOUR, PAL24(0xA6BF00), PAL24(0x00FFFF), // yellow & cyan
+    BGCOLOUR, GRIDCOLOUR, PAL24(0x610069), PAL24(0x6000D6), // dark purple & purple
+};
+
+const uint16_t objpal[8*4] = {
+    PAL24(0x000000), PAL24(0x000000), PAL24(0x000000), PAL24(0xFFFFFF),
+    PAL24(0x000000), PAL24(0x000000), PAL24(0x000000), PAL24(0x999999),
+    PAL24(0x000000), PAL24(0x000000), PAL24(0x000000), PAL24(0x444444),
+    PAL24(0x000000), PAL24(0x000000), PAL24(0x000000), PAL24(0x000000),
+    PAL24(0x000000), PAL24(0x000000), PAL24(0x000000), PAL24(0x000000),
+    PAL24(0x000000), PAL24(0x000000), PAL24(0x000000), PAL24(0x000000),
+    PAL24(0x000000), PAL24(0x000000), PAL24(0x000000), PAL24(0x000000),
+    PAL24(0x000000), PAL24(0x000000), PAL24(0x000000), PAL24(0x000000),
+};
+
+void genConnectedNodeTiles(uint8_t* startPtr, uint8_t* endPtr, bool useAltColour)
+{
+    if (useAltColour)
+    {
+        // change 01 to 10
+        for (uint8_t* i = startPtr; i < endPtr; i += 16) // top
+        {
+            *(i) &= ~0b00011000;
+            (*(i + 1)) |= 0b00011000;
+        }
+        for (uint8_t* i = startPtr + 0x010E; i < endPtr + 0x010E; i += 16) // bottom
+        {
+            (*i) &= ~0b00011000;
+            (*(i + 1)) |= 0b00011000;
+        }
+        for (uint8_t* i = startPtr + 0x0206; i < endPtr + 0x0206; i += 16) // left
+        {
+            (*i) &= ~0b10000000;
+            (*(i + 1)) |= 0b10000000;
+            (*(i + 2)) &= ~0b10000000;
+            (*(i + 3)) |= 0b10000000;
+        }
+        for (uint8_t* i = startPtr + 0x0306; i < endPtr + 0x0306; i += 16) // right
+        {
+            (*i) &= ~0b00000001;
+            (*(i + 1)) |= 0b00000001;
+            (*(i + 2)) &= ~0b00000001;
+            (*(i + 3)) |= 0b00000001;
+        }
+    }
+    else
+    {
+        // some of the writes aren't needed since there's already a grid box drawn around the node
+        // it doesn't need to change 00 to 11, just 01 to 11
+        for (uint8_t* i = startPtr; i < endPtr; i += 16) // top
+        {
+            //(*i) |= 0b00011000;
+            (*(i + 1)) |= 0b00011000;
+        }
+        for (uint8_t* i = startPtr + 0x010E; i < endPtr + 0x010E; i += 16) // bottom
+        {
+            //(*i) |= 0b00011000;
+            (*(i + 1)) |= 0b00011000;
+        }
+        for (uint8_t* i = startPtr + 0x0206; i < endPtr + 0x0206; i += 16) // left
+        {
+            //(*i) |= 0b10000000;
+            (*(i + 1)) |= 0b10000000;
+            //(*(i + 2)) |= 0b10000000;
+            (*(i + 3)) |= 0b10000000;
+        }
+        for (uint8_t* i = startPtr + 0x0306; i < endPtr + 0x0306; i += 16) // right
+        {
+            //(*i) |= 0b00000001;
+            (*(i + 1)) |= 0b00000001;
+            //(*(i + 2)) |= 0b00000001;
+            (*(i + 3)) |= 0b00000001;
+        }
+    }
+}
 
 void main()
 {
-    runGame();
+    lcd_off(); // Disable screen so we can copy to VRAM freely
+
+    memcpy((void*)0x8000, cursorTiles, cursorTiles_end - cursorTiles);
+    memcpy((void*)0x8800, backgroundTiles, backgroundTiles_end - backgroundTiles);
+    memcpy((void*)0x8900, connectionTiles, connectionTiles_end - connectionTiles);
+    memcpy((void*)0x8B00, nodeShapeTiles, nodeShapeTiles_end - nodeShapeTiles); // unconnected
+
+    // Setup VRAM for CGB
+    if (cpu_type == CPU_CGB)
+    {
+        cgb_background_palette(bgpal);
+        cgb_object_palette(objpal);
+
+        memcpy((void*)0x8A00, nodeNumberTilesCGB, nodeNumberTilesCGB_end - nodeNumberTilesCGB);
+
+        // Copy in the tiles for the alternate colour connections
+        memcpy((void*)0x9000, connectionTiles, connectionTiles_end - connectionTiles);
+
+        // Convert the colours for the alternate colour connections
+        for (uint8_t* i = (uint8_t*)0x9000; i < (uint8_t*)(0x9000 + (connectionTiles_end - connectionTiles)); i += 2)
+        {
+            for (uint8_t a = 1; a != 0; a <<= 1)
+            {
+                if (((*i) & a) && ((*(i + 1)) & a))
+                {
+                    (*i) &= ~a;
+                }
+            }
+        }
+
+        // Convert the colours for the alternate colour shape nodes
+        for (uint8_t* i = (uint8_t*)0x8B80; i < (uint8_t*)(0x8B80 + 16 * 8); i += 2)
+        {
+            for (uint8_t a = 1; a != 0; a <<= 1)
+            {
+                if (((*i) & a) && ((*(i + 1)) & a))
+                {
+                    (*i) &= ~a;
+                }
+            }
+        }
+
+        memcpy((void*)0x8C00, (void*)0x8B00, nodeShapeTiles_end - nodeShapeTiles); // top
+        memcpy((void*)0x8D00, (void*)0x8B00, nodeShapeTiles_end - nodeShapeTiles); // bottom
+        memcpy((void*)0x8E00, (void*)0x8B00, nodeShapeTiles_end - nodeShapeTiles); // left
+        memcpy((void*)0x8F00, (void*)0x8B00, nodeShapeTiles_end - nodeShapeTiles); // right
+
+        // Generate the "connected" tiles for nodeShapes
+        genConnectedNodeTiles((uint8_t*)0x8C00, (uint8_t*)0x8C80, false);
+        genConnectedNodeTiles((uint8_t*)0x8C80, (uint8_t*)0x8D00, true);
+    }
+    else // DMG
+    {
+        rBGP = 0b11100100;
+        rOBP0 = 0b11100100;
+        rOBP1 = 0b11100100;
+
+        memcpy((void*)0x8A00, nodeNumberTiles, nodeNumberTiles_end - nodeNumberTiles);
+
+        memcpy((void*)0x8C00, (void*)0x8B00, nodeShapeTiles_end - nodeShapeTiles); // top
+        memcpy((void*)0x8D00, (void*)0x8B00, nodeShapeTiles_end - nodeShapeTiles); // bottom
+        memcpy((void*)0x8E00, (void*)0x8B00, nodeShapeTiles_end - nodeShapeTiles); // left
+        memcpy((void*)0x8F00, (void*)0x8B00, nodeShapeTiles_end - nodeShapeTiles); // right
+
+        // Generate the "connected" tiles for nodeShapes
+        genConnectedNodeTiles((uint8_t*)0x8C00, (uint8_t*)0x8D00, false);
+    }
+
+    // Setup main menu graphics
+    memcpy((void*)0x9100, mainMenuTiles, mainMenuTiles_end - mainMenuTiles);
+    uint8_t* dstPtr = (uint8_t*)0x9800;
+    const uint8_t* srcPtr = mainMenuTilemap;
+    for (uint8_t y = 0; y < 18; y++)
+    {
+        for (uint8_t x = 0; x < 20; x++)
+        {
+            // 0x10 = tile offset from tilemap
+            (*dstPtr++) = (*srcPtr++) + 0x10;
+        }
+        dstPtr += 12;
+    }
+    rSCY = 0;
+    rSCX = 0;
+
+    // Setup the OAM for sprite drawing
+    oam_init();
+
+    // Make sure sprites and the background are drawn (also turns the screen on)
+    rLCDC = LCDC_ON | LCDC_OBJON | LCDC_BGON;
+
+    // Setup the VBLANK interrupt.
+    rIF = 0;
+    rIE = IE_VBLANK;
+    ENABLE_INTERRUPTS();
+
+    // Init joypad state
+    joypad_state = 0;
+
+    while(1)
+    {
+        joypad_update();
+
+        if (joypad_pressed & PAD_START)
+        {
+            runGame();
+        }
+    }
+}
+
+ISR_VBLANK()
+{
+    oam_dma_copy();
+    updateCursorAnimation();
 }
