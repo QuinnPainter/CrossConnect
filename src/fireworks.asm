bcdf4d5a07e22fb35400af31d277acabc1f247c1
quinnpainter <quinn@geekymonkey.com>
2022-04-29 16:34:36 +0100

    actual circular fireworks

--- src/fireworks.asm
+++ src/fireworks.asm
@@ -12,9 +12,9 @@ DEF FIREWORK_SIZE RB 0
 
 DEF FIREWORK_TILEINDEX EQU $02
 DEF FIREWORK_FIRST_SPRITE EQU 1 ; Sprite index of the first firework sprite
-DEF NUM_FIREWORK_SPRITES EQU 1
-DEF AIR_RESISTANCE EQU $0019 ; Amount the firework X velocity decreases by every frame. 8.8 fixed
-DEF GRAVITY EQU $0023 ; Amount the firework Y velocity decreases by each frame. 8.8 fixed
+DEF NUM_FIREWORK_SPRITES EQU 14
+DEF AIR_RESISTANCE EQU $0011 ; Amount the firework X velocity decreases by every frame. 8.8 fixed
+DEF GRAVITY EQU $0015 ; Amount the firework Y velocity decreases by each frame. 8.8 fixed
 
 SECTION "FireworksRAM", WRAM0
 wFireworkArray: DS FIREWORK_SIZE * NUM_FIREWORK_SPRITES
@@ -22,28 +22,62 @@ wFireworkArray: DS FIREWORK_SIZE * NUM_FIREWORK_SPRITES
 SECTION "FireworksHRAM", HRAM
 hFireworkSpritePtr: DB
 
+SECTION "FireworkVelocityTable", ROM0
+; angles are measured 0 - 65536.0 in rgbasm
+DEF YVEL_MULTIPLIER EQU 160000
+DEF XVEL_MULTIPLIER EQU 200000
+DEF NUM_DIRECTIONS EQU 14
+ANGLE = 0.0
+REPT NUM_DIRECTIONS
+YVEL = MUL(SIN(ANGLE), YVEL_MULTIPLIER) ; 16.16 fixed point number
+    db (YVEL >> 16) & $FF ; save integer part
+    db (YVEL >> 8) & $FF ; save fractional part
+XVEL = MUL(COS(ANGLE), XVEL_MULTIPLIER) ; 16.16 fixed point number
+    db (XVEL >> 16) & $FF ; save integer part
+    db (XVEL >> 8) & $FF ; save fractional part
+ANGLE = ANGLE + ((65536 / NUM_DIRECTIONS) << 16) ; left shift to convert to fixed point
+ENDR
+PRINTLN ANGLE
+
 
 SECTION "FireworksCode", ROM0
 _startFireworks::
     ld hl, wFireworkArray
-    ld a, $50
-    ld [hli], a
+    ld bc, STARTOF("FireworkVelocityTable")
+    ld d, NUM_FIREWORK_SPRITES
+.setupArrayLp:
+    ld a, $50   ;
     ld [hli], a ; ypos
-    ld a, $04
-    ld [hli], a
-    ld [hli], a ; yvel
-
-    ld a, $50
-    ld [hli], a
+    ld [hli], a ;
+    ld a, [bc]      ;
+    inc bc          ;
+    ld [hli], a     ; yvel
+    ld a, [bc]      ;
+    inc bc          ;
+    ld [hli], a     ;
+    ld a, $50   ;
     ld [hli], a ; xpos
-    ld a, $04
-    ld [hli], a
-    ld [hli], a ; xvel
+    ld [hli], a ;
+    ld a, [bc]      ;
+    inc bc          ;
+    ld [hli], a     ; xvel
+    ld a, [bc]      ;
+    inc bc          ;
+    ld [hli], a     ;
+    dec d
+    jr nz, .setupArrayLp
 
+    ld hl, wShadowOAM + (FIREWORK_FIRST_SPRITE * sizeof_OAM_ATTRS) + OAMA_TILEID
+    ld b, NUM_FIREWORK_SPRITES
+.setupSpritesLp:
     ld a, FIREWORK_TILEINDEX
-    ld [wShadowOAM + (FIREWORK_FIRST_SPRITE * sizeof_OAM_ATTRS) + OAMA_TILEID], a
-    xor a
-    ld [wShadowOAM + (FIREWORK_FIRST_SPRITE * sizeof_OAM_ATTRS) + OAMA_FLAGS], a
+    ld [hli], a
+    xor a ; set sprite attibutes to 0
+    ld [hli], a
+    inc hl
+    inc hl
+    dec b
+    jr nz, .setupSpritesLp
 
     ret
 
@@ -75,8 +109,15 @@ _updateFireworks::
     ld a, c
     ld [hli], a
 
+    ; Set sprite Y
+    ldh a, [hFireworkSpritePtr]
+    ld c, a
     ld a, b
-    ld [wShadowOAM + (FIREWORK_FIRST_SPRITE * sizeof_OAM_ATTRS) + OAMA_Y], a
+    ld b, HIGH(wShadowOAM)
+    ld [bc], a
+    ld a, c
+    inc a
+    ldh [hFireworkSpritePtr], a
 
     ; Apply gravity to Y Position
     push hl
@@ -113,8 +154,15 @@ _updateFireworks::
     ld a, c
     ld [hli], a
 
+    ; Set sprite X
+    ldh a, [hFireworkSpritePtr]
+    ld c, a
     ld a, b
-    ld [wShadowOAM + (FIREWORK_FIRST_SPRITE * sizeof_OAM_ATTRS) + OAMA_X], a
+    ld b, HIGH(wShadowOAM)
+    ld [bc], a
+    ld a, c
+    add 3 ; skip over tile and attribute, go to next sprite Y pos
+    ldh [hFireworkSpritePtr], a
 
     ; Apply air resistance to X velocity
     push hl
@@ -140,4 +188,9 @@ _updateFireworks::
     ld [hli], a
     ld a, e
     ld [hli], a
+
+    ; Check if we've done all the particles yet
+    ldh a, [hFireworkSpritePtr]
+    cp (FIREWORK_FIRST_SPRITE + NUM_FIREWORK_SPRITES) * sizeof_OAM_ATTRS
+    jr c, .mainlp
     ret
